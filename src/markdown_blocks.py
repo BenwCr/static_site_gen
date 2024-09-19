6599999bbd60614c39870ab402cd7e9974020300
BenwCr <bwcrouse15@gmail.com>
2024-09-19 07:58:30 -0500

    markdown_blocks done

--- src/markdown_blocks.py
+++ src/markdown_blocks.py
@@ -1,5 +1,12 @@
 import re
 
+block_type_paragraph = "paragraph"
+block_type_heading = "heading"
+block_type_code = "code"
+block_type_quote = "quote"
+block_type_olist = "ordered_list"
+block_type_ulist = "unordered_list"
+
 def markdown_to_blocks(markdown):
     blocks = markdown.split("\n\n")
     filtered_blocks = []
@@ -12,16 +19,16 @@ def markdown_to_blocks(markdown):
 
 def block_to_block_type(block):
     if len(re.findall(r"^(#{1,6})\s",block)) != 0:
-        return "heading"
+        return block_type_heading
     elif re.match(r"^```[\s\S]*```$", block) != None:
-        return "code"
+        return block_type_code
     elif re.match(r"^(> .*\n?)+$", block) != None:
-        return "quote"
+        return block_type_quote
     elif re.match(r"^(\* .*\n|-\s.*\n)*(\* .*$|-\s.*)$", block) != None:
-        return "unordered_list"
+        return block_type_ulist
     elif is_ordered_list(block) == True:
-        return "ordered_list"
-    return "paragraph"
+        return block_type_olist
+    return block_type_paragraph
 
 def is_ordered_list(block):
     lines = block.strip().split('\n')
--- src/test_markdown_blocks.py
+++ src/test_markdown_blocks.py
@@ -48,29 +48,29 @@ This is the same paragraph on a new line
 
     def test_block_to_block(self):
         block = "#### This is a Heading"
-        self.assertEqual(block_to_block_type(block), "heading")
+        self.assertEqual(block_to_block_type(block), block_type_heading)
     def test_block_to_block1(self):
         block = "``` This is a Heading```"
-        self.assertEqual(block_to_block_type(block), "code")
+        self.assertEqual(block_to_block_type(block), block_type_code)
     def test_block_to_block2(self):
         block = "This is a paragraph"
-        self.assertEqual(block_to_block_type(block), "paragraph")
+        self.assertEqual(block_to_block_type(block), block_type_paragraph)
     def test_block_to_block3(self):
         block = """> This as a Quote
 > Quote"""
-        self.assertEqual(block_to_block_type(block), "quote")
+        self.assertEqual(block_to_block_type(block), block_type_quote)
     def test_block_to_block4(self):
         block = """* List
 * List2"""
-        self.assertEqual(block_to_block_type(block), "unordered_list")
+        self.assertEqual(block_to_block_type(block), block_type_ulist)
     def test_block_to_block45(self):
         block = """- List
 - List2"""
-        self.assertEqual(block_to_block_type(block), "unordered_list")
+        self.assertEqual(block_to_block_type(block), block_type_ulist)
     def test_block_to_block(self):
         block = """1. item
 2. item
 3. item"""
-        self.assertEqual(block_to_block_type(block), "ordered_list")
+        self.assertEqual(block_to_block_type(block), block_type_olist)
 if __name__ == "__main__":
     unittest.main()
